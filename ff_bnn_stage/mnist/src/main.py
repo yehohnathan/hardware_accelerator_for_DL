12ecf457ef78aea1b89d59e0596a8a1f469bc083
yehohnathan <yehohnathan2002@gmail.com>
2026-03-16 09:42:08 -0600

    Imprime ejemplos de binarios para contrastar con los resultados obtenidos en la FPGA
    Signed-off-by: yehohnathan <yehohnathan2002@gmail.com>

--- ff_bnn_stage/mnist/src/main.py
+++ ff_bnn_stage/mnist/src/main.py
@@ -10,11 +10,11 @@ Este script:
 8. Guarda archivos .npy para depuración.
 """
 # =============================== Librerías ================================= #
-import numpy as np                  # Cálculo numérico eficiente
+# import numpy as np                  # Cálculo numérico eficiente
 
 # =========================== Importar Funciones ============================ #
-import mnist_binarize_onehot_pack as mnist_utils
-
+# import mnist_binarize_onehot_pack as mnist_utils
+import read_bin
 
 # =========================== Variables globales ============================ #
 # Tamaño lateral de la imagen MNIST: 28x28
@@ -41,92 +41,19 @@ FF_PACK_WORDS = (FF_INPUT_BITS + PACK_BITS - 1) // PACK_BITS
 
 # ================================== Main =================================== #
 if __name__ == "__main__":
-    input_path = "data\\raw\\train.csv"
-    # Ruta del archivo de entrada
+    input_path = "data/processed/mnist_ff_input_packed.bin"
+    sample_idx = 2
 
-    # Carga el dataset original desde archivo
-    print("\n# 1. Cargando dataset MNIST #")
-    X, y = mnist_utils.load_train_table(input_path)
+    labels, packed = read_bin.load_packed_binary_dataset(input_path,
+                                                         FF_PACK_WORDS)
 
-    print(f"Dataset cargado: {X.shape[0]} muestras.")
-    print(f"Etiquetas cargadas: {y.shape[0]} muestras.")
-    print(f"{X.shape[1]} píxeles por imagen.")
+    read_bin.print_full_sample(packed, sample_idx=sample_idx)
 
-    # Revisa estructura, dimensiones y rangos
-    print("\n# 2. Revisando estructura, dimensiones y rangos #")
-    mnist_utils.validate_dataset(X, y)
+    read_bin.print_first_words(packed, sample_idx=sample_idx)
 
-    # Convierte los píxeles de 0-255 a bits 0/1
-    print("\n# 3. Binarizando píxeles #")
-    threshold = 127     # Umbral de binarización
-    X_bin = mnist_utils.binarize_pixels(X, threshold=threshold,
-                                        show_process=False, sample_idx=0,)
+    read_bin.print_first_bits(packed, sample_idx=sample_idx)
 
-    # Convierte cada etiqueta escalar en vector one-hot
-    print("\n# 4. Convirtiendo etiquetas a one-hot #")
-    y_onehot = mnist_utils.labels_to_onehot(y, num_classes=NUM_CLASSES)
-    print(f"Ejemplo de etiqueta one-hot: {y_onehot[42]}"
-          f" (para etiqueta original {y[42]})")
+    labels, packed = read_bin.load_packed_binary_dataset(input_path,
+                                                         PIX_PACK_WORDS)
 
-    # Concatena imagen binaria + etiqueta one-hot
-    print("\n# 5. Concatenando imagen binaria + etiqueta one-hot #")
-    X_ff = mnist_utils.concatenate_ff_input(X_bin, y_onehot,
-                                            onehot_position="prefix",)
-    print(f"y_onehot[42]:   {y_onehot[42]} | y[42]: {y[42]}")
-    print(f"EX_ff[42][:20]: {X_ff[42][:20]}")
-
-    # Empaqueta solo los píxeles binarios
-    print("\n# 6. Empaquetando bits en palabras de 32 bits #")
-    X_pix_packed = mnist_utils.pack_bits_matrix(X_bin, total_bits=NUM_PIXELS,
-                                                pack_bits=PACK_BITS,)
-
-    # Comprobar consistencia posterior al empaquetado de píxeles
-    print("\n# 7. Comprobando consistencia posterior al empaquetado #")
-    mnist_utils.verify_bit_packing(X_bin, X_pix_packed, total_bits=NUM_PIXELS,
-                                   pack_bits=PACK_BITS,)
-
-    print("\n# 8. Empaquetando (+ ONEHOTENCODE) bits en palabras de 32 bits #")
-    # Empaqueta la entrada completa para Forward-Forward
-    X_ff_packed = mnist_utils.pack_bits_matrix(X_ff, total_bits=FF_INPUT_BITS,
-                                               pack_bits=PACK_BITS,)
-
-    # Comprobar consistencia posterior al empaquetado de píxeles
-    print("\n# 9. Comprobando consistencia posterior al empaquetado"
-          " (+ ONEHOTENCODE) #")
-    mnist_utils.verify_bit_packing(X_ff, X_ff_packed, total_bits=FF_INPUT_BITS,
-                                   pack_bits=PACK_BITS,)
-
-    # Comprobación final: contraste en bruto
-    print("\n# 10. Comprobación final: contraste en bruto #")
-    X_unpacked_42 = mnist_utils.unpack_bits_row(X_ff_packed[42],
-                                                total_bits=FF_INPUT_BITS,
-                                                pack_bits=PACK_BITS,)
-    print(f"X_ff_packed[42]: {X_ff_packed[42]}")
-    # print(f"X_ff[42]: {X_ff[42]}")
-    # print(f"X_unpacked_42: {X_unpacked_42}")
-    # X_ff[42][3] = 22    # Modificación para verificar que detecta diferencias
-    print(f"X_ff == X_unpacked_42: {np.array_equal(X_ff[42], X_unpacked_42)}")
-
-    # Guarda el archivo binario con solo píxeles empaquetados
-    mnist_utils.save_packed_pixels_dataset(
-        "data\\processed\\mnist_bnn_pixels_packed.bin", X_pix_packed, y,)
-
-    # Guarda el archivo binario con entrada FF empaquetada
-    mnist_utils.save_packed_ff_dataset(
-        "data\\processed\\mnist_ff_input_packed.bin", X_ff_packed, y,)
-
-    # Guarda archivos intermedios en formato .npy para depuración
-    mnist_utils.save_numpy_debug(
-        "data\\processed\\mnist", X=X, y=y, X_bin=X_bin, y_onehot=y_onehot,
-        X_ff=X_ff, X_pix_packed=X_pix_packed, X_ff_packed=X_ff_packed,)
-
-    # Prueba de lectura y desempaquetado para verificar integridad
-    """
-    labels, packed, X_rec = mnist_utils.test_binary_dataset(
-        "data\\processed\\mnist_bnn_pixels_packed.bin",
-        packed_words=PIX_PACK_WORDS, total_bits=NUM_PIXELS)
-
-    labels, packed, X_rec = mnist_utils.test_binary_dataset(
-        "data\\processed\\mnist_ff_input_packed.bin",
-        packed_words=FF_PACK_WORDS, total_bits=FF_INPUT_BITS)
-    """
+    read_bin.inspect_sample_structure(packed, sample_idx=sample_idx)
